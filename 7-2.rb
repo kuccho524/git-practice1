b2084432652e4797a66f4864440562b34e282f05
kuccho524 <kucchachachanocha@gmail.com>
2021-04-24 11:13:24 +0900

    commit

---
+++ 7-2.rb
@@ -0,0 +1,20 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか"
+count = gets.to_i
+i = 1
+
+while count >= i
+  puts "#{i}回目の計算"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+  i += 1
+end
+
+puts "計算を終了します"
